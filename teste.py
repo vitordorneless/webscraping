c6fad17dacca9974f56562374f463605212ea360
vitordorneless <vitordorneles@hotmail.com>
2023-04-16 21:52:39 -0300

    testando webscraping para pegar resultados

--- teste.py
+++ teste.py
@@ -1,1 +1,18 @@
-print('entrei')
+from bs4 import BeautifulSoup
+import requests
+
+html = requests.get("https://www.sorteonline.com.br/resultados").text
+
+soup = BeautifulSoup(html, 'html.parser')
+
+temperaturas = soup.find('div','card lot-megasena')
+
+uls = temperaturas.find('ul')
+
+for ul in uls:
+    print(ul.get_text())
+
+
+
+#for temperatura in temperaturas:
+#    print(temperatura.get_text())
